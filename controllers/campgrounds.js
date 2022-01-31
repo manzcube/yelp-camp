038c64f41a23d805f248194760569ded1c136943
Marc Alzamora <marcalzamora1@hotmail.com>
2022-01-31 20:12:12 +0100

    implementing maps with mapbox and their geocoding api

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,6 +1,9 @@
 
 const Campground = require('../models/campground')
 const { cloudinary } = require('../cloudinary')
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding")
+const mapBoxToken = process.env.MAPBOX_TOKEN
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken })
 
 
 module.exports.index = async (req, res) => {
@@ -13,7 +16,12 @@ module.exports.renderNewForm = (req, res) => {
 }
 
 module.exports.createCampground = async (req, res, next) => {
+    const geoData = await geocoder.forwardGeocode({
+        query: req.body.campground.location,
+        limit: 1
+    }).send()
     const campground = new Campground(req.body.campground)
+    campground.geometry = geoData.body.features[0].geometry
     campground.images = req.files.map(f => ({url: f.path, filename: f.filename}))
     campground.author = req.user._id
     await campground.save()
@@ -49,7 +57,7 @@ module.exports.renderEditForm = async (req, res) => {
 module.exports.updateCampground = async (req, res) => {
     const { id } = req.params
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
-    const imgs = req.files.map(f => ({url: f.path, filename: f.filename}))
+    const imgs = req.files.map(f => ({url: f.path, filename: f.filename}))   // Solev problem of geometry require type foe editing campground
     campground.images.push(...imgs)
     if (req.body.deleteImages) {
         for(let filename of req.body.deleteImages) {
---
+++ public/js/showPageMap.js
@@ -0,0 +1,11 @@
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+container: 'map', 
+style: 'mapbox://styles/mapbox/streets-v11', 
+center: campground.geometry.coordinates,
+zoom: 10
+});
+
+new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .addTo(map) 
